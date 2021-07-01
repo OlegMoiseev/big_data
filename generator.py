35b9dac2fce5012299194f5f1be2203ad488d02d
Олег Моисеев <olegmoiseev2698@gmail.com>
2021-07-01 22:13:31 +0300

    new branch

--- generator.py
+++ generator.py
@@ -1,23 +1,26 @@
 import numpy as np
-import mmap
 
 
-def generate(size: int, filename: str):
-    """Function for generating file with numbers uint32 written in big endian. Return sum of numbers, min and max number in array"""
-    with open(filename, 'wb') as f_writer:
-        generated_array = np.random.randint(0, 2**32, size//4, dtype=np.dtype('uint32')).newbyteorder()
-        f_writer.write(generated_array.data)
+def generate_rand_int48():
+    randByte1 = np.random.randint(1,255).to_bytes(1, byteorder='little')
+    randByte2 = np.random.randint(1,255).to_bytes(1, byteorder='little')
+    randByte3 = np.random.randint(1,255).to_bytes(1, byteorder='little')
+    randByte4 = np.random.randint(1,255).to_bytes(1, byteorder='little')
+    randByte5 = np.random.randint(1,255).to_bytes(1, byteorder='little')
+    randByte6 = np.random.randint(1,255).to_bytes(1, byteorder='little')
 
-def read_f(filename: str):
-    with open(filename, 'rb') as f_reader:
-        return np.frombuffer(f_reader.read(), dtype=np.dtype('uint32').newbyteorder('>'))
+    randBytes = randByte1 + randByte2 + randByte3 + randByte4 + randByte5 + randByte6
+    randInt48 = int.from_bytes(randBytes, byteorder='little')
 
-def read_mmap_f(size: int, filename: str):
-    with open(filename, 'rb') as f_reader:
-        mm_buf = mmap.mmap(f_reader .fileno(), length=size, offset=0, access=mmap.ACCESS_READ)
-        return np.frombuffer(mm_buf, dtype=np.dtype('uint32').newbyteorder('>'))
+    return randInt48
 
-def disp_res(min_elem: int, max_elem: int, sum_elems: int, ):
-    print(f"Min: ", min_elem)
-    print(f"Max: ", max_elem)
-    print(f"Sum: ", sum_elems)
+
+def create_file_with_int48(num_int, f_name):
+    with open(f_name, 'w') as file_writer:
+        generated_array = np.array([generate_rand_int48() for _ in range(num_int)])
+        np.savetxt(file_writer, generated_array, fmt="%u")
+
+
+def read_f(f_name):
+    with open(f_name, 'rb') as f_reader:
+        return np.fromiter(f_reader.readlines(), dtype=np.dtype('u8'))
